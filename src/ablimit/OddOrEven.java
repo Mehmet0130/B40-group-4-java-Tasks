9e1d148245e34b29cd6e1c9abb0b26233029362c
ablim <ablimit630@hotmail.ca>
2025-08-05 15:32:44 -0400

    Add oddOrEven method created

---
+++ src/ablimit/OddOrEven.java
@@ -0,0 +1,14 @@
+package ablimit;
+
+public class OddOrEven {
+
+        public static void oddOrEven ( int num){
+            if (num % 2 == 0) {
+                System.out.println(num + " is even number");
+            } else {
+                System.out.println(num + " is odd number");
+            }
+
+
+        }
+    }
